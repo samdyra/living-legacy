6882a872cf0802c16b0a7538527f3a2dbd91dba5
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2022-07-30 10:13:05 +0700

    boiler plate

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,2 +1,1 @@
 export { default as HomeScreen } from "./Home";
-export { default as KaryaScreen } from "./Karya";
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
   </React.StrictMode>
 );
